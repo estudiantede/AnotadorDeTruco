1ba639b8efb8bdce15255d8bae424b7ef57a5f35
Inaki Frutos <inakifrutos00@gmail.com>
2023-10-20 18:15:55 -0300

    Imported class

--- Interfaz.py
+++ Interfaz.py
@@ -1,7 +1,9 @@
-from guizero import App, Text
+from guizero import App, Text, TextBox, Box, Combo, Picture, PushButton, Slider
 
-ventana = App("Hola mundo", height=200, bg="#FFFFFF")
-texto = Text(ventana, "Esto es un texto")
 
+ventana = App("Hola mundo", height=500, bg="#FFFFFF")
+titulo = Text(ventana, "Anotador de truco", size=30, width="fill", color="#FFBBCC", height=3)
+nombre1 = TextBox(ventana, width="fill", height=2)
+nombre2 = TextBox(ventana, width="fill", height=2)
 
 ventana.display()

750fca0a3331514ad671158888d884c9f029d497
estudiantede <99504012+estudiantede@users.noreply.github.com>
2023-10-26 10:54:22 -0300

    Update Interfaz.py

--- Interfaz.py
+++ Interfaz.py
@@ -34,3 +34,45 @@ jug1 = Combo(ventana, options=cantosTruco)
 jug2 = Combo(ventana, options=cantosEnvido)
 
 ventana.display()
+
+from guizero import App, Text, TextBox, Box, Combo, Picture, PushButton, Slider
+
+puntos = [0, 0]
+
+def SumarPunto (num):
+    #print("Hola mundo")
+    if num==0:
+        puntos[0] += 1
+        puntosJug1.value = str(puntos[0])
+    elif num==1:
+        puntos[1] += 1
+        puntosJug2.value = str(puntos[1])
+
+cantosTruco = ["(Elegir opcion) ⬇️", "Nada","Truco","Retruco","Vale cuatro"]
+cantosEnvido = ["(Elegir opcion) ⬇️","Envido","Real envido","Falta envido"]
+participante = ["(Elegir opcion) ⬇️","Jugador Uno","Jugador Dos"]
+ventana = App("Anotador de truco", height=700, bg="#FFFFFF")
+
+titulo = Text(ventana, "Anotador de truco", size=30, width="fill", color="#FFBBCC", height=3, align="top")
+centerBox1 = Box(ventana, width="fill", border=True)
+centerBox2 = Box(ventana, width="fill", border=True)
+centerBox3 = Box(ventana, width="fill", border=True)
+centerBox4 = Box(ventana, width="fill", border=True)
+
+nombre1 = TextBox(centerBox1, width="fill", align="left")
+nombre2 = TextBox(centerBox1, width="fill", align="right")
+
+#imagenJug1 = Picture(ventana, image="images/1.jpeg")
+#imagenJug2 = Picture(ventana, image="images/1.jpeg")
+
+puntosJug1 = Text(centerBox2, text="0", align="left")
+puntosJug2 = Text(centerBox2, text="0", align="right")
+
+sumarPuntosJug1 = PushButton(centerBox3, command=SumarPunto, args=[0], align="left")
+sumarPuntosJug2 = PushButton(centerBox3, command=SumarPunto, args=[1], align="right")
+jug1 = Combo(centerBox4, options=cantosTruco, align="left")
+jug2 = Combo(centerBox4, options=cantosEnvido, align="right")
+wchos = Combo(ventana, options=participante)
+Puntos = PushButton(ventana, command=SumarPunto, args=[1])
+
+ventana.display()

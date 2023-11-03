45d1550a56820e785ba4e40b42bd1e6a8c9cfce7
estudiantede <inakifrutos00@gmail.com>
2023-11-03 15:22:53 -0300

    Se agrega esto

--- Interfaz.py
+++ Interfaz.py
@@ -1,6 +1,27 @@
 from guizero import App, Text, TextBox, Box, Combo, Picture, PushButton, Slider
-
+import funciones
 puntos = [0, 0]
+
+def sumarPuntosJug1():
+    puntos[0] += 1
+    puntosJug1.value = str(puntos[0])
+
+def restarPuntosJug1():
+    if puntos[0] > 0:
+        puntos[0] -= 1
+        puntosJug1.value = str(puntos[0])
+
+def sumarPuntosJug2():
+    puntos[1] += 1
+    puntosJug2.value = str(puntos[1])
+
+def restarPuntosJug2():
+    if puntos[1] > 0:
+        puntos[1] -= 1
+        puntosJug2.value = str(puntos[1])
+
+def faltaEnvido():
+    print("hola mundo")
 
 def SumarPunto (num):
     #print("Hola mundo")
@@ -11,6 +32,10 @@ def SumarPunto (num):
         puntos[1] += 1
         puntosJug2.value = str(puntos[1])
 
+def keys(event):
+    if(event.keycode == 18): #cuando se presiona alt
+        print("hola")
+
 cantosTruco = ["(Elegir opcion) ⬇️", "Nada","Truco","Retruco","Vale cuatro"]
 cantosEnvido = ["(Elegir opcion) ⬇️","Envido","Real envido","Falta envido"]
 participante = ["(Elegir opcion) ⬇️","Jugador Uno","Jugador Dos"]
@@ -18,15 +43,15 @@ ventana = App("Anotador de truco", height=700, bg="#FFFFFF")
 
 titulo = Text(ventana, "Anotador de truco", size=30, width="fill", color="#A6F3EB", height=1, align="top")
 
-centerBox1 = Box(ventana, width="fill", border=True, height=50) 
-centerBoxPicture = Box(ventana, width="fill", border=True, height=50) 
-centerBox2 = Box(ventana, width="fill", border=True, height=50)
-centerBox3 = Box(ventana, width="fill", border=True, height=50)
-centerBox4 = Box(ventana, width="fill", border=True, height=50)
-centerBox5 = Box(ventana, width="fill", border=True, height=50)
+centerBox1 = Box(ventana, width="fill", height=50) 
+centerBoxPicture = Box(ventana, width="fill", height=50) 
+centerBox2 = Box(ventana, width="fill", height=50)
+centerBox3 = Box(ventana, width="fill", height=50)
+centerBox4 = Box(ventana, width="fill", height=50)
+centerBox5 = Box(ventana, width="fill", height=50)
 
-nombre1 = TextBox(centerBox1, width="fill", align="left")
-nombre2 = TextBox(centerBox1, width="fill", align="right")
+nombre1 = TextBox(centerBox1, width="25", align="left")
+nombre2 = TextBox(centerBox1, width="25", align="right")
 
 #imagenJug1 = Picture(ventana, image="images/1.jpeg")
 #imagenJug2 = Picture(ventana, image="images/1.jpeg")
@@ -34,15 +59,23 @@ nombre2 = TextBox(centerBox1, width="fill", align="right")
 puntosJug1 = Text(centerBox2, text="0", align="left")
 puntosJug2 = Text(centerBox2, text="0", align="right")
 
-sumarPuntosJug1 = PushButton(centerBox3, text="+Puntos", command=SumarPunto, args=[0], align="left")
-sumarPuntosJug2 = PushButton(centerBox3, text="+Puntos", command=SumarPunto, args=[1], align="right")
+
+
+agregarPuntosJug2 = PushButton(centerBox3, text="+Puntos", command=sumarPuntosJug1, align="left")
+agregarPuntosJug1 = PushButton(centerBox3, text="+Puntos", command=sumarPuntosJug2, align="right")
 TrucoJug1 = Combo(centerBox4, options=cantosTruco, align="left")
 TrucoJug2 = Combo(centerBox4, options=cantosTruco, align="right")
 
 Envidojug1 = Combo(centerBox5, options=cantosEnvido, align="left")
 Envidojug2 = Combo(centerBox5, options=cantosEnvido, align="right")
 #wchos = Combo(ventana, options=participante)
-PuntosJug1 = PushButton(ventana, text="Sumar Puntos", command=SumarPunto, args=[1])
+
+puntosJug1.when_clicked = sumarPuntosJug1
+puntosJug2.when_clicked = sumarPuntosJug2
+
+puntosJug1.when_right_button_pressed = restarPuntosJug1
+puntosJug2.when_right_button_pressed = restarPuntosJug2
+
+ventana.when_key_released = keys
 
 ventana.display()
-
